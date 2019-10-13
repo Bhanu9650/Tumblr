bce597500a59a71424ba9ae6fae97f98a318c455
Bhanu9650 <bhanugupta.im@gmail.com>
2019-10-13 15:56:58 +0530

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_075921) do
+ActiveRecord::Schema.define(version: 2019_10_13_092308) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
